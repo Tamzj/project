75e68b8e2d2f53d27cbd8fd18b97c184d2b17265
Tamzj <2973259626@qq.com>
2018-02-03 21:07:59 +0800

    2018/2/2修改

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 //创建一个任务
-gulp.task('compileSass',function(){
+gulp.task('cs',function(){
     //查找文件
     gulp.src('./src/sass/*.scss')
 
     //管道传输，编译
-    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+    .pipe(sass({outputStyle:'compact'}).on('error', sass.logError))
 
     //输出到硬盘
     .pipe(gulp.dest('./src/css/'))
@@ -16,20 +16,20 @@ gulp.task('compileSass',function(){
 
 
 //自动刷新页面
-var browserSync = require('browserSync');
+// var browserSync = require('browserSync');
 
-gulp.task('serv',function(){
+// gulp.task('server',function(){
     //启动一个自动刷新的服务器
-    browserSync({
+    // browserSync({
         //创建一个静态服务器
-        server:'./src',
+        // server:'./src',
         //指定端口
-        port:1008,
+        // port:1008,
 
         //监听文件修改
-        files:['./src/**/*.html','./src/css/*.css']
+        // files:['./src/**/*.html','./src/css/*.css']
 
-    });
+    // });
     //监听sass修改
-    gulp.watch('./src/sass/*.scss',['compileSass'])
-})
+    // gulp.watch('./src/sass/*.scss',['compileSass'])
+// })
