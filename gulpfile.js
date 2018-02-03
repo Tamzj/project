5de5fb692f14a9a0f8ed71f3e9bb41c449310f92
Tamzj <2973259626@qq.com>
2018-02-03 17:25:07 +0800

    做完头部、尾部公共部分html和样式

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+//引入gulp
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//创建一个任务
+gulp.task('compileSass',function(){
+    //查找文件
+    gulp.src('./src/sass/*.scss')
+
+    //管道传输，编译
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+
+    //输出到硬盘
+    .pipe(gulp.dest('./src/css/'))
+})
+
+
+//自动刷新页面
+var browserSync = require('browserSync');
+
+gulp.task('serv',function(){
+    //启动一个自动刷新的服务器
+    browserSync({
+        //创建一个静态服务器
+        server:'./src',
+        //指定端口
+        port:1008,
+
+        //监听文件修改
+        files:['./src/**/*.html','./src/css/*.css']
+
+    });
+    //监听sass修改
+    gulp.watch('./src/sass/*.scss',['compileSass'])
+})
